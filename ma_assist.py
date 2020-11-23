aef5ab4b0ce3f13685c7665bce92c62e8db87800
michaelheichler <michaelheichler@protonmail.com>
2020-11-23 14:13:33 +0100

    Init2

--- ma_assist.py
+++ ma_assist.py
@@ -1,6 +1,6 @@
 #################
 # BWI Data Analytics Hackathon 2020
-# MachineLearning Assistant via GPT-2
+# Machine Build Assistant via GPT-2
 # Pierre Prange-Schmunk
 # Michael Heichler
 #################
