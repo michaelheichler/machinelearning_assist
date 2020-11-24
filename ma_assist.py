881c39d98c46bfdeb4b08986affcae97d4dd1cb0
michaelheichler <michaelheichler@protonmail.com>
2020-11-24 14:44:08 +0100

    Interact + Persona

---
+++ ma_assist.py
@@ -0,0 +1,10 @@
+#################
+# BWI Data Analytics Hackathon 2020
+# Machine Build Assistant via GPT-2
+# Pierre Prange-Schmunk
+# Michael Heichler
+#################
+
+# Import Packages
+import torch as tp
+import 
